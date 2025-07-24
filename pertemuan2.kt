c8c721afa050bc48c14d4bd8261b4baef0a1250e
Abriansyah Adam <abriansyahadam5@gmail.com>
2025-07-24 18:59:01 +0700

    readonly list

--- pertemuan2.kt
+++ pertemuan2.kt
@@ -1,8 +1,18 @@
+// Class
 class Contact(val id: Int, var email: String)
+
+// Contoh List
+fun contohList() {
+    // read only list
+    val readOnly = listOf("me", "hello", "apa", "kabar")
+    println(readOnly)
+}
 
 fun main() {
     val contact = Contact(1, "hello@mail.com")
-    println(contact.email)
+    // println(contact.email)
     contact.email = "giveonaldo@gmail.com"
-    println(contact.email)
+    // println(contact.email)
+
+    contohList()
 }
