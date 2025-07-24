a0fbd26a676a41563a9253833d5c72144d4d9b91
Abriansyah Adam <abriansyahadam5@gmail.com>
2025-07-24 18:55:54 +0700

    memanggil class  Contact

--- pertemuan2.kt
+++ pertemuan2.kt
@@ -2,4 +2,7 @@ class Contact(val id: Int, var email: String)
 
 fun main() {
     val contact = Contact(1, "hello@mail.com")
+    println(contact.email)
+    contact.email = "giveonaldo@gmail.com"
+    println(contact.email)
 }
